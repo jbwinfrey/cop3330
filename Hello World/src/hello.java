636d0ce72fd0790b54832166d83b6d422afbada5
jbwinfrey <jbwinfrey@users.noreply.github.com>
2019-09-05 23:24:32 -0400

    Update hello.java

--- Hello World/src/hello.java
+++ Hello World/src/hello.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 public class hello {
 	 public static void main (String [] args) {
@@ -7,3 +8,14 @@ public class hello {
 	   }
 
 }
+=======
+
+public class hello {
+	 public static void main (String [] args) {
+
+	      System.out.print("Hello, World!");
+	   
+	   }
+
+}
+>>>>>>> a2d605c94a55c823831b7b19b22c67f04456fdf7
