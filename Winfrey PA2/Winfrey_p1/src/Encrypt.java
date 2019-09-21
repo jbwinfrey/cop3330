1eaa78bec316c047d5740103b0a57ca55dce4e0e
jbwinfrey <jbwinfrey@users.noreply.github.com>
2019-09-21 18:21:13 -0400

    Programming Assignment 2

---
+++ Winfrey PA2/Winfrey_p1/src/Encrypt.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Encrypt {
+	 public static void main (String [] args) {
+		 Scanner scnr = new Scanner(System.in);
+		 
+	     int v1;
+	     int v2;
+	     int v3;
+	     int v4;
+	     int v1tmp;
+	     int v2tmp;
+	     int v3tmp;
+	     int v4tmp;
+	     
+	     System.out.println("Enter Data (Seperate Each Digit With Enter):");
+	     
+	     v1 = scnr.nextInt();    
+	     v2 = scnr.nextInt();	     
+	     v3 = scnr.nextInt();
+	     v4 = scnr.nextInt();
+	     	 
+	     v1tmp = v1 + 7;
+	     v2tmp = v2 + 7;
+	     v3tmp = v3 + 7;
+	     v4tmp = v4 + 7;
+	     	     
+	     v1tmp = v1tmp % 10;
+	     v2tmp = v2tmp % 10;
+	     v3tmp = v3tmp % 10;
+	     v4tmp = v4tmp % 10;
+	     	     
+	     v1 = v3tmp;
+	     v2 = v4tmp;
+	     v3 = v1tmp;
+	     v4 = v2tmp;
+	     
+	     System.out.println("Encrypted Value: " + v1 +" "+ v2 +" "+ v3 +" "+ v4);
+  
+	     
+	     
+	     
+	 }
+
+}
