98570b7da447a32da017167e163d56eb0179df6f
jbwinfrey <jbwinfrey@users.noreply.github.com>
2019-09-21 17:38:08 -0400

    p1

---
+++ Winfrey PA2/src/decrypt.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class decrypt {
+	 public static void main (String [] args) {
+		 Scanner scnr = new Scanner(System.in);
+		 
+	     int v1;
+	     int v2;
+	     int v3;
+	     int v4;
+	     int v1tmp;
+	     int v2tmp;
+	     int v3tmp;
+	     int v4tmp;
+	     
+	     System.out.println("Enter Encrypted Value (Seperate Each Digit With Enter): ");
+	     
+	     v1 = scnr.nextInt();    
+	     v2 = scnr.nextInt();	     
+	     v3 = scnr.nextInt();
+	     v4 = scnr.nextInt();
+	     
+	     v3tmp = v1;
+	     v4tmp = v2;
+	     v1tmp = v3;
+	     v2tmp = v4;
+	     	     
+	     
+	     if (v1tmp<7) {
+	    	 v1 = v1tmp + 10 ;
+	    	 }
+	    	else {
+	    	v1 = v1tmp;
+	    	}
+	        
+	     if (v2tmp<7) {
+	    	 v2 = v2tmp + 10 ;
+	    	 }
+	    	else {
+	    	v2 = v2tmp;
+	    	}
+	     
+	     if (v3tmp<7) {
+	    	 v3 = v3tmp + 10 ;
+	    	 }
+	    	else {
+	    	v3 = v3tmp;
+	    	}
+	     
+	     if (v4tmp<7) {
+	    	 v4 = v4tmp + 10 ;
+	    	 }
+	    	else {
+	    	v4 = v4tmp;
+	    	}
+	      
+	     
+	     v1 = v1 - 7;
+	     v2 = v2 - 7;
+	     v3 = v3 - 7;
+	     v4 = v4 - 7;
+	     
+	     
+	    System.out.println("Original Value: " + v1 +" "+ v2 +" "+ v3 +" "+ v4);
+
+	     
+	     
+	     
+	     
+	     
+	     
+	 }
+
+}
