74eab0cceeab2b0d62b3987ad73f301c96686078
jbwinfrey <jbwinfrey@users.noreply.github.com>
2019-09-21 16:43:40 -0400

    p3

--- Programming Assignment 2/src/Poll.java
+++ Programming Assignment 2/src/Poll.java
@@ -1,36 +1,73 @@
 import java.util.Scanner;
+import java.util.Random;
 
 
 public class Poll {
 	
 	public static void main(String[] args) {
-	     
 		Scanner scnr = new Scanner(System.in);
-	      final int NUM_ELEMENTS = 8;             // Number of elements
-	      int[] userVals = new int[NUM_ELEMENTS]; // Array of user numbers
-	      int i;                                  // Loop index
-	      int maxVal;                             // Computed max
-
-	      // Prompt user to populate array
-	      System.out.println("Enter " + NUM_ELEMENTS + " integer values...");
-	      
-	      for (i = 0; i < userVals.length; ++i) {
-	         System.out.print("Value: ");
-	         userVals[i] = scnr.nextInt();
-	      }
-
-	      // Determine largest (max) number
-	      maxVal = userVals[0];                   // Largest so far
-	      
-	      for (i = 0; i < userVals.length; ++i) {
-	         if (userVals[i] > maxVal) {
-	            maxVal = userVals[i];
-	         }
-	      }
-	      System.out.println("Max: " + maxVal);
-	   }
+		
+		Random randGen = new Random();
+		
+		//String done = "NotDone";
+		int numUsers = 10;
+		int rating = 0;
+		int curUser;
+		int i;
+		int numTopics = 5;
+		float topicAvg = 0; 
+				
 	
-    
+		String[] topics = new String[numTopics];
+		int[][] response = new int[numTopics][numUsers];
+		int avg[] = new int[numTopics];
+		
+		
+		topics[0] = "Topic 1";
+		topics[1] = "Topic 2";
+		topics[2] = "Topic 3";
+		topics[3] = "Topic 4";
+		topics[4] = "Topic 5";
+		
+				
+		 //System.out.println(topics[0]);
+		 
+		 for (curUser = 1; curUser <= numUsers; curUser = curUser + 1) {
+	      
+			for (i = 0; i < numTopics; i = i + 1) {
+				
+			System.out.println("User " + curUser);	
+		    System.out.println(topics[i]);
+			 
+			System.out.print("Rate this Topic: ");
+ 		 	rating = randGen.nextInt(10); //scnr.nextInt() ;
+ 		 	response [i][curUser-1] = rating;
+ 		 	
+			}
+ 		 	
+		 }
+		 
+		 
+		 System.out.println("\n\n        |  User 1  |  User 2  |  User 3  |  User 4  |  User 5  |  User 6  |  User 7  |  User 8  |  User 9  |  User 10 |  Average");
+		 
+		 for (i = 0; i < 5; i = i + 1) {
+			 
+			 	System.out.print(topics[i]);
+			 
+			 for (curUser = 0; curUser < numUsers; curUser = curUser + 1) {
+		 
+				 System.out.print(" |    "+ response[i][curUser] + "    ");
+				 
+				 avg[i] = response[i][curUser];
+				 topicAvg = topicAvg + avg[i];
+				 
+		 }
+			 	topicAvg = topicAvg / numUsers;
+			 System.out.print(" |     "+ topicAvg +"\n");
+			 topicAvg = 0;
+		 }
     
 
 }
+}
+
