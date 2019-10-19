bf317ea88933e37525d85be492616c5e3f3abdcc
jbwinfrey <jbwinfrey@users.noreply.github.com>
2019-10-19 12:52:21 -0400

    winfrey PA3

--- Winfrey PA3/winfrey_p2/src/Main.java
+++ Winfrey PA3/winfrey_p2/src/Main.java
@@ -2,8 +2,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Problem 2");
+        double ratePer1 = 4, ratePer2 = 5;
+        double rate1 = ratePer1 / 100, rate2 = ratePer2 / 100;
+        double strtAmt1 = 2000, strtAmt2 = 3000;
 
+            SavingsAccount saver1 = new SavingsAccount(strtAmt1);
+            SavingsAccount saver2 = new SavingsAccount(strtAmt2);
 
+            System.out.println("Month | Saver1 Total | Saver2 Total");
+
+            for(int i=1; i<=12; ++i){
+                SavingsAccount.modifyInterestRate(rate1);
+
+                saver1.calculateMonthlyInterest();
+                saver2.calculateMonthlyInterest();
+
+                System.out.print(i + "     |   " );
+                saver1.printBalance();
+                System.out.print("   |   ");
+                saver2.printBalance();
+                System.out.println();
+
+            }
+
+            SavingsAccount.modifyInterestRate(rate2);
+
+                    System.out.println("Next Month at 5%: \t");
+                System.out.print("Saver1: ");
+                saver1.calculateMonthlyInterest();
+                saver1.printBalance();
+                    System.out.println();
+                System.out.print("Saver2: ");
+                saver2.calculateMonthlyInterest();
+                saver2.printBalance();
+
+        }
     }
-}
--- Winfrey PA3/winfrey_p2/src/SavingsAccount.java
+++ Winfrey PA3/winfrey_p2/src/SavingsAccount.java
@@ -1,2 +1,31 @@
 public class SavingsAccount {
+
+    public static double annualInterestRate = 0.0;
+
+    private double savingsBalance;
+
+
+    public SavingsAccount(double total){
+        this.savingsBalance = total;
+    }
+
+    public void calculateMonthlyInterest(){
+        double monthlyInterest;
+        monthlyInterest = (this.savingsBalance * annualInterestRate)/12;
+        this.savingsBalance += monthlyInterest;
+    }
+
+    public static void modifyInterestRate(double newRate){
+        annualInterestRate = newRate;
+    }
+
+    public void printBalance(){
+        System.out.printf("$%.2f", this.savingsBalance);
+    }
+
+
+
 }
+
+
+
